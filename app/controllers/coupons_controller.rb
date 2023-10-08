a607233f5cd1e8f64c3d4d9fa8788d2cbe65018d
Zinni Chang <czinni853@gmail.com>
2023-10-08 23:46:46 +0800

    [build]: add notice style and coupons controller

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,46 @@
+class CouponsController < ApplicationController
+  before_action :find_coupon, only: [:edit, :update, :destroy]
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = Coupon.new(book_params)
+    if @coupon.save
+      redirect_to coupons_path, notice: "新增成功"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @coupon.update(book_params)
+      redirect_to coupons_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @coupon.destroy
+    redirect_to coupons_path, notice: "已刪除"
+  end
+
+  private
+  def find_coupon
+    @coupon = Coupon.find_by(id: params[:id])
+    redirect_to coupons_path, notice: "無此商品" unless @coupon
+  end
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :discount_amount, :expiration_date, :max_usage_count, :used_count, :is_used)
+  end
+end
